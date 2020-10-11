ff63a9ae12904068a869325602ce04a3ac31f5e5
Erik Maide <maide.erik@gmail.com>
2020-10-11 23:51:36 +0300

    some minor changes

--- KytsiKalku/Models/FuelData.cs
+++ KytsiKalku/Models/FuelData.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections;
 namespace KytsiKalku.Models
 {
     public class FuelData
