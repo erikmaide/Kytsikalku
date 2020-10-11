85b81a8c04b09394e4b3a40246efbc977f76db9d
Erik Maide <maide.erik@gmail.com>
2020-10-11 10:42:48 +0300

    remove helloworld controller etc

--- KytsiKalku/Models/FuelData.cs
+++ KytsiKalku/Models/FuelData.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 namespace KytsiKalku.Models
 {
     public class FuelData
     {
         public int Id { get; set; }
+
+
+
+        [Display(Name = "Driving destination from start to end")]
+        [Required]
         public string TripName { get; set; }
 
+        [Display(Name = "Date")]
         [DataType(DataType.Date)]
+        [Required]
         public DateTime DriveDate { get; set; }
+
+        [Display(Name = "Initial mileage")]
+        [Required]
         public int TripStart { get; set; }
+        [Display(Name = "Final mileage")]
+        [Required]
         public int TripEnd { get; set; }
     }
 }
